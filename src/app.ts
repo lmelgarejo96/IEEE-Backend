abaf1dec59b1b6eabdf54f6ab48e205bc3941125
Luis <luismelgarejoalarcon@gmail.com>
2020-01-18 01:14:42 -0500

    Auth + token + cookie complete

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import authRoutes from './routes/auth';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'; 
 
 const app: Application = express();
 
@@ -15,10 +16,20 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
+app.use(cors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:8080',
+    ],
+    credentials: true
+}));
 
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/', (req, res) => {
+    res.send('hello world');
+})
 
 // Static files
 
--- src/services/service.auth.ts
+++ src/services/service.auth.ts
@@ -11,6 +11,8 @@ import { UsuarioController } from '../controllers/usuario.controller';
 const LOGINCONTROLLER = new LoginController();
 const USERCONTROLLER = new UsuarioController();
 
+const EXPIRATION = 60 * 60 * 5; //5 horas
+
 // Register
 export const signup = async (req: Request, res: Response) => {
     const CORREO = await USERCONTROLLER.buscarPorCorreo(req.body.Correo);
@@ -38,32 +40,37 @@ export const signup = async (req: Request, res: Response) => {
 
 // Login
 export const signin = async (req: Request, res: Response) => { 
+    try {
+        const LOGIN: Login = await LOGINCONTROLLER.buscarPorCorreo(req.body.Correo);
+        if(!LOGIN) return res.status(403).json('Email or password is wrong'); 
     
-    const LOGIN: Login = await LOGINCONTROLLER.buscarPorCorreo(req.body.Correo);
-    if(!LOGIN) return res.status(403).json('Email or password is wrong'); 
-
-    const CORRECTPASS: boolean = await LOGIN.validarContraseña(req.body.Contraseña);
-    if(!CORRECTPASS) return res.status(403).json('Invalid password'); 
-
-    if( LOGIN.getEstadoConfirmacion() === false || 
-        LOGIN.getEstadoTabla() === true ) return res.status(401).json('Access denied');
+        const CORRECTPASS: boolean = await LOGIN.validarContraseña(req.body.Contraseña);
+        if(!CORRECTPASS) return res.status(403).json('Invalid password'); 
     
-    const TOKEN: string = await jwt.sign({IdUsuario: LOGIN.getIdUsuario()}, process.env.SECRET_TOKEN || 'whatevertoken', {
-        expiresIn: 60*60*5 // 5 hours
-    });
+        if( LOGIN.getEstadoConfirmacion() === false || 
+            LOGIN.getEstadoTabla() === true ) return res.status(401).json('Access denied');
+        
+        const TOKEN: string = await jwt.sign({IdUsuario: LOGIN.getIdUsuario()}, process.env.SECRET_TOKEN || 'whatevertoken', {
+            expiresIn: EXPIRATION// 5 hours
+        });
+        console.log(TOKEN)
+        
+        res.cookie('access_token', TOKEN, {
+            maxAge: EXPIRATION,
+            expires: new Date(Date.now() + EXPIRATION),
+            httpOnly: true,
+            secure: false // cambiar a true cuando se use https
+        });
+        res.status(200).json(LOGIN);
+    } catch (error) {
+        console.log(error)
+    }
 
-    res.cookie('access_token', TOKEN, {
-        maxAge: 60*60*5,
-        httpOnly: true,
-        secure: true
-    });
-
-    res.status(200).json(LOGIN);
 }
 
 // Profile info
 export const profile = async (req: Request, res: Response) => {
-
+    
     const USER = await USERCONTROLLER.usuarioPorId(parseInt(req.UserId));
     if(!USER) return res.status(404).json('No user found');
     res.json(USER);
